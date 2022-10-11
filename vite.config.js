f9cb3d6aa9873aa7e20e29d1a23606a4f635d61e
Mathilde <mathilde@krogen5.dk>
2022-10-11 12:05:51 +0200

    om os side + lidt ekstra

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        // about: resolve(__dirname, "src/about.html"),
+        omos: resolve(__dirname, "src/omos.html"),
+
       },
     },
   },
