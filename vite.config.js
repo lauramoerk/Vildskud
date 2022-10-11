6fc606cff90dc5338441d82bfce7cafc25a0f80b
Laura <lauramoerk@hotmail.com>
2022-10-11 12:56:28 +0200

    fjerne iconify

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,12 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         omos: resolve(__dirname, "src/omos.html"),
+        events: resolve(__dirname, "src/events.html"),
+        forestillinger: resolve(__dirname, "src/forestillinger.html"),
+        frivillige: resolve(__dirname, "src/frivillige.html"),
+        opencall: resolve(__dirname, "src/opencall.html"),
+        programoversigt: resolve(__dirname, "src/programoversigt.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
 
       },
     },
