7a2947b28ca0d2c8dceae3879bc597f70888c48c
Laura <lauramoerk@hotmail.com>
2022-10-12 15:56:45 +0200

    fjerne alpine

--- vite.config.js
+++ vite.config.js
@@ -17,10 +17,18 @@ export default defineConfig({
         omos: resolve(__dirname, "src/omos.html"),
         events: resolve(__dirname, "src/events.html"),
         forestillinger: resolve(__dirname, "src/forestillinger.html"),
+        gogoyoku: resolve(__dirname, "src/gogoyo-ku.html"),
+        mandliggersommandselvharredt: resolve(__dirname, "src/mandliggersommandharredt.html"),
+        vildmedmig: resolve(__dirname, "src/vildmedmig.html"),
+        detlevenderum: resolve(__dirname, "src/detlevenderum.html"),
+        frygtenforikkeatleve: resolve(__dirname, "src/frygtenforikkeatleve.html"),
+        whatfirestogetherwirestogether: resolve(__dirname, "src/whatfirestogetherwirestogether.html"),
+        rebirth: resolve(__dirname, "src/re-birth.html"),
         frivillige: resolve(__dirname, "src/frivillige.html"),
         opencall: resolve(__dirname, "src/opencall.html"),
         programoversigt: resolve(__dirname, "src/programoversigt.html"),
         sitemap: resolve(__dirname, "src/sitemap.html"),
+
 
       },
     },
