65b4d3862675ef0351839cb5a07fdde80b1bf26a
Laura <lauramoerk@hotmail.com>
2022-10-12 07:28:20 +0200

    please fyld mindre?

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,16 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors:{
+      'bgcolor':"#FEF6EC",
+      'navcolor':"#F7E6CF",
+      'primarycolor':"#EB9210",
+      'accentcolor':"#C93139",
+      'white':"#FFFFFF",
+      'black':"#000",
+    },
+    fontFamily:{
+      custard: "'custard',sans-serif",
+      urbanist: "'Urbanist', sans-serif",},
     extend: {
-      colors:{
-        bgcolor:"#FEF6EC",
-        navcolor:"#F7E6CF",
-        primary:"#EB9210",
-        accent:"#C93139",
-      },
-      fontFamily:{
-        custard: "'custard',sans-serif",
-        urbanist: "'Urbanist', sans-serif",}
+
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("daisyui")], 
